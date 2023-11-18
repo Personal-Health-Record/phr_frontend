5fed6a8656c100bc7c3c8799ad148b7d61976417
arinugraha11 <ari.nugraha@tokopedia.com>
2023-11-18 17:29:57 +0700

    feat: integrate name in homepage

--- src/routes/HomePage/components/HeaderHome/index.tsx
+++ src/routes/HomePage/components/HeaderHome/index.tsx
@@ -1,11 +1,14 @@
 import docter2Img from "../../../../assets/images/docter-2.png";
 import profileIconImg from "../../../../assets/images/profile-icon.png";
+import { useGetLoggedInUser } from "../../../../helpers/userDataHelper";
 
 interface Props {
   isDoctor: boolean;
 }
 
 const HeaderHome = ({ isDoctor }: Props) => {
+  const { loggedInUser } = useGetLoggedInUser();
+
   return (
     <div className="flex border-b-2 py-3 px-4 items-center gap-5">
       <img
@@ -14,7 +17,9 @@ const HeaderHome = ({ isDoctor }: Props) => {
         width={50}
         height={50}
       />
-      <h3 className="text-2xl font-bold text-mainGrey">Halo, Raihan</h3>
+      <h3 className="text-2xl font-bold text-mainGrey">
+        Halo, {loggedInUser?.name}
+      </h3>
     </div>
   );
 };
