747d595c54819e1908b182f74ee021530bbf5a85
Muhammad Erlangga <muhammad.erlangga@oyindonesia.com>
2023-12-12 19:35:02 +0700

    adjust shadow vaksinasi card

--- src/routes/VaksinasiPage/components/VaksinasiCard/index.tsx
+++ src/routes/VaksinasiPage/components/VaksinasiCard/index.tsx
@@ -10,9 +10,10 @@ const VaksinasiCard = ({ isComplete, title, handleClick }: Props) => {
   return (
     <div
       onClick={handleClick}
-      className={`flex items-center justify-center shadow-lg rounded-2xl px-2 py-5 text-center`}
+      className={`flex items-center justify-center rounded-2xl px-2 py-5 text-center`}
       style={{
         backgroundColor: isComplete ? '#9FDBFD' : 'white',
+        boxShadow: '0px 1px 8px 0px rgba(0, 0, 0, 0.2)',
       }}
     >
       <p className="text-xs">{title}</p>
