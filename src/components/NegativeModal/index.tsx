e7245a73f97efb465df9763d66cae11c8193b9d4
Muhammad Erlangga <muhammad.erlangga@oyindonesia.com>
2023-12-16 09:13:19 +0700

    move modal to center

--- src/components/NegativeModal/index.tsx
+++ src/components/NegativeModal/index.tsx
@@ -10,14 +10,17 @@ type NegativeModalProps = {
 const NegativeModal: React.FC<NegativeModalProps> = ({ description, onClose, onSubmit, show }) => {
   return (
     <div>
-      {show &&
-        <div
-          id="popup-modal"
-          tabIndex={-1}
-          className="overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full"
-        >
-          <div className="relative p-4 w-full max-w-md max-h-full">
-            <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
+      {show && (
+        <>
+          <div
+            className="fixed inset-0 bg-gray-800 bg-opacity-50 z-40"
+            onClick={onClose} // Close the modal when clicking outside the modal
+          ></div>
+          <div
+            id="popup-modal"
+            className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-50"
+          >
+            <div className="relative p-4 w-full max-w-md max-h-full bg-white rounded-lg shadow dark:bg-gray-700">
               <button
                 type="button"
                 className="absolute top-3 end-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
@@ -79,8 +82,8 @@ const NegativeModal: React.FC<NegativeModalProps> = ({ description, onClose, onS
               </div>
             </div>
           </div>
-        </div>
-      }
+        </>
+      )}
     </div>
   );
 };
