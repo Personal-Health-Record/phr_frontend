1adc40eff4de9bb5f3f2e2220bec715020efaa03
nugrahaa878 <nugrahaa878@gmail.com>
2024-05-29 18:24:46 +0700

    feat: finish ui tracking detial faskes

--- src/routes/MonitoringDetailPage/index.tsx
+++ src/routes/MonitoringDetailPage/index.tsx
@@ -1,9 +1,31 @@
 import Header from "../../components/Header";
+import BarChartSection from "../MonitoringPage/components/BarChartSection";
+import CardKonsumsiMakanan from "../MonitoringPage/components/CardKonsumsiMakanan";
 
 const MonitoringDetailPage = () => {
   return (
     <div className="flex flex-col">
       <Header title="Tracking Data Kesehatan" />
+
+      <div className="p-3">
+        <p className="text-xs text-mainGrey">Tanggal Tracking Data Kesehatan</p>
+
+        <div className="flex border-b-2 py-2 items-center justify-between mb-3">
+          <div className="flex flex-col gap-1">
+            <p className="text-sm text-lightGrey">
+              11 April 2024 - 14 April 2024
+            </p>
+          </div>
+        </div>
+
+        <BarChartSection
+          activeIdx={1}
+          startDate={new Date()}
+          endDate={new Date()}
+        />
+
+        <CardKonsumsiMakanan startDate={new Date()} endDate={new Date()} />
+      </div>
     </div>
   );
 };
