10b4adcd61ad85a0f8812309e7af7fc1cbb932ad
nugrahaa878 <nugrahaa878@gmail.com>
2024-05-29 18:34:29 +0700

    feat: implement filter

--- src/routes/MonitoringListPage/index.tsx
+++ src/routes/MonitoringListPage/index.tsx
@@ -1,11 +1,25 @@
+import { useState } from "react";
 import Header from "../../components/Header";
 import { dummyListTrackingKesehatanFaskes } from "../../helpers/constants";
 import CardMonitoring from "./components/CardMonitoring";
 import SearchBar from "./components/SearchBar";
 
 const MonitoringListPage = () => {
+  const [listTrackingData, setListTrackingData] = useState(
+    dummyListTrackingKesehatanFaskes
+  );
+
   const onSearch = (keyword: string) => {
-    console.log(keyword);
+    if (keyword === "") {
+      setListTrackingData(dummyListTrackingKesehatanFaskes);
+      return;
+    }
+
+    const filteredTrackingData = listTrackingData.filter((data) =>
+      data.toLowerCase().includes(keyword.toLowerCase())
+    );
+
+    setListTrackingData(filteredTrackingData);
   };
 
   return (
@@ -14,7 +28,7 @@ const MonitoringListPage = () => {
       <div className="flex flex-col w-full px-4 py-4 gap-3">
         <SearchBar onSearch={onSearch} />
 
-        {dummyListTrackingKesehatanFaskes.map((dateMonitoring) => (
+        {listTrackingData.map((dateMonitoring) => (
           <CardMonitoring date={dateMonitoring} />
         ))}
       </div>
